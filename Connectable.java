8756808fb6dba457260b38b73be87f86f6a9686b
bjagodzinski <b.s.jagodzinski@gmail.com>
2016-08-16 07:53:54 +0200

    Update Connectable.java

--- Connectable.java
+++ Connectable.java
@@ -7,6 +7,10 @@ import java.net.HttpURLConnection;
  * Created by Rafal on 2016-08-15.
  */
 public interface Connectable {
+    
+    // cosider not void methods
+    
+    // maybe it it worth to return coooection when you connect? or sth else
     void URLConnect() throws IOException;
     void URLDisconnect();
 }
